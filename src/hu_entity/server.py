bc17d31840b890fc96cc6c05adda6fec6ca66d05
Paul Annetts <paul.annetts@hutoma.com>
2017-05-10 13:55:15 +0000

    Merged PR 1026: Add logging support from common Python code.
    Add logging support from common Python code.
    Small refactors to help this, or some issues pylint was flagging.
    
    Related work items: #1899

--- src/hu_entity/server.py
+++ src/hu_entity/server.py
@@ -1,20 +1,30 @@
+"""The named entity recognizer service"""
 import argparse
 import json
 import os
-from aiohttp import web
 from pathlib import Path
 
+from aiohttp import web
+
 import spacy
-import en_core_web_md
 import spacy.matcher
+import en_core_web_md
 
 from hu_entity.named_entity import NamedEntity
 
+import hu_logging
+
 DATA_DIR = Path(os.path.dirname(os.path.realpath(__file__)) + '/data')
+
+def _get_logger():
+    logger = hu_logging.get_logger('hu_entity.server')
+    return logger
 
 class EntityRecognizerServer:
 
     def __init__(self):
+        self.logger = _get_logger()
+        self.logger.warning("Loading model...")
         # reads the spacy model
         self.nlp = en_core_web_md.load()
         # initialize the matcher with the model just read
@@ -64,6 +74,7 @@ class EntityRecognizerServer:
 
         # reads the city file
         city_path = DATA_DIR/'cities1000.txt'
+        self.logger.warning('Add custom locations from %s', city_path)
         with city_path.open(encoding='utf8') as fp:
             for line in fp:
                 columns = line.split('\t')
@@ -99,7 +110,9 @@ class EntityRecognizerServer:
         '''
         the function returns a collection of recognized entities as JSON response
         '''
-        entities = self.get_entities(request.rel_url.query['q'])
+        q = request.rel_url.query['q']
+        self.logger.info("Entity request '%s'" % q)
+        entities = self.get_entities(q)
         resp = web.Response()
         resp.text = json.dumps(entities)
         resp.content_type = 'application/json'
@@ -109,16 +122,20 @@ class EntityRecognizerServer:
 
 def main():
     """Main function"""
+    hu_logging.initialize_logging('/tmp/hu_entity_log', "NER")
+    logger = _get_logger()
+    logger.warning("Entity Recognizer booting up.")
     parser = argparse.ArgumentParser(description="NER server")
     parser.add_argument('--port', type=int, default=9095)
     args = parser.parse_args()
+    port = args.port
 
     er_server = EntityRecognizerServer()
-
     er_server.initialize_NER_with_custom_locations()
     app = web.Application()
     app.router.add_route('GET', '/ner', er_server.handle)
-    web.run_app(app, port=args.port)
+    logger.warning("Starting entity recognizer API on port %d", port)
+    web.run_app(app, port=port)
 
 
 if __name__ == '__main__':
