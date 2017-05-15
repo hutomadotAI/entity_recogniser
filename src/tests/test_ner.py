e258113540f8a627fe784f6a2c0570e0363f1c00
Paul Annetts <paul.annetts@hutoma.com>
2017-05-15 14:35:44 +0100

    Fix tests Add a person category test

--- src/tests/test_ner.py
+++ src/tests/test_ner.py
@@ -13,13 +13,31 @@ def ner_server():
 
 def test_recognize_location(ner_server):
     entity_list = ner_server.get_entities("Reading")
-    assert entity_list[0].label == 'Reading'
-    assert entity_list[0].named_entity == 'GPE'
+    entity = entity_list[0] 
+    assert entity.entity_value == 'Reading'
+    assert entity.spacy_category == 'custom_cities'
+    assert entity.category == 'sys.geopolitical'
+    assert entity.start_loc == 0
+    assert entity.end_loc == 7
 
 def test_recognize_date(ner_server):
     entity_list = ner_server.get_entities("I would like to see you today")
-    assert entity_list[0].label == 'today'
-    assert entity_list[0].named_entity == 'DATE'
+    entity = entity_list[0]
+    assert entity.entity_value == 'today'
+    assert entity.spacy_category == 'DATE'
+    assert entity.category == 'sys.date'
+    assert entity.start_loc == 24
+    assert entity.end_loc == 29
+
+def test_recognize_person(ner_server):
+    entity_list = ner_server.get_entities("Who is Sherlock Holmes")
+    entity = entity_list[0]
+    assert entity.entity_value == 'Sherlock Holmes'
+    assert entity.spacy_category == 'PERSON'
+    assert entity.category == 'sys.person'
+    assert entity.start_loc == 7
+    assert entity.end_loc == 22
+
 
 @pytest.fixture()
 def cli(loop, test_client, ner_server):
@@ -44,8 +62,10 @@ async def test_server_ner_q_1(cli):
     assert isinstance(json_resp, list)
     assert len(json_resp) == 1
     item = json_resp[0]
-    assert item['named_entity'] == "GPE"
-    assert item['label'] == "Reading"
+    assert item['category'] == "sys.geopolitical"
+    assert item['value'] == "Reading"
+    assert item['start'] == 0
+    assert item['end'] == 7
 
 async def test_server_ner_no_entity(cli):
     resp = await cli.get('/ner?q=Nothing')
@@ -61,8 +81,12 @@ async def test_server_ner_multi_instance(cli):
     assert isinstance(json_resp, list)
     assert len(json_resp) == 2
     item = json_resp[0]
-    assert item['named_entity'] == "GPE"
-    assert item['label'] == "Reading"
+    assert item['category'] == "sys.geopolitical"
+    assert item['value'] == "Reading"
+    assert item['start'] == 22
+    assert item['end'] == 29
     item = json_resp[1]
-    assert item['named_entity'] == "DATE"
-    assert item['label'] == "tomorrow"
+    assert item['category'] == "sys.date"
+    assert item['value'] == "tomorrow"
+    assert item['start'] == 30
+    assert item['end'] == 38
